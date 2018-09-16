96a68134d2049e720b04112f03eb98610f1b6d3a
W. Joel Schneider <w.joel.schneider@gmail.com>
2018-09-16 11:07:20 -0400

    delete old documentation

--- R/spirograph.R
+++ R/spirograph.R
@@ -1,22 +1,23 @@
 #' Make coordinates for spirograph
-#'
 #' @param fixed_radius The radius of the fixed circle being cycled around. Can be positive or negative.
 #' @param cycling_radius The radius of the moving circle cycling around the fixed circle. Can be positive or negative.
 #' @param pen_radius The pen placement inside the moving circle cycling around the fixed circle. Can be positive or negative.
+#' @param windings The number of times the the moving circle winds around the fixed circle.
+#' @param color_groups = Number of groups in group_id variable
+#' @param colors Vector of colors for groups. Defaults to viridis scale.
+#' @param transparency Transparency of fills. Ranges from 0 to 1. Defaults to 1.
+#' @param color_cycles The number of times the color vector  is recycled
 #' @param origin_x The x coordinate of the center of the fixed circle
 #' @param origin_y The y coordinate of the center of the fixed circle
-#' @param windings The number of times the the moving circle winds around the fixed circle.
 #' @param rotation The number of radians by which the entire figure should be rotated around the origin of the fixed circle
 #' @param start_angle The number of radians on the fixed circle's arc where the moving circle starts cycling.
-#' @param npoints = The number of points to generate
-#' @param color_groups = Number of groups in group_id variable
-#' @param color Vector of colors for groups. Defaults to viridis scale.
-#' @param transparency Transparency of fills. Ranges from 0 to 1. Defaults to 1.
+#' @param points_per_polygon = The number of points for each color grouping
 #' @param rule Character value specifying the path fill mode: either "evenodd" or "winding". Defaults to "evenodd"
-#' @param openfile Open file in default program for .svg format. Defaults to TRUE
+#' @param openfile Open file in default program for .svg format. Defaults to TRUE.
 #' @param aspect_ratio Aspect ratio of the image. Defaults to 1.
-#' @param path Name of the output file. Defaults to "spiro.svg"
-
+#' @param end_at_beginning Adds the first point to the end of the figure but with the colors of the last color group. Defaults to FALSE.
+#' @param savefile Save a file. Defaults to TRUE.
+#' @param filename Name of the output file. Defaults to "spiro.svg"
 #' @return A tibble with x and y coordinates of the spirograph, row id, and group id
 #' @examples
 #' spirograph(fixed_radius = 3, cycling_radius = 1)
@@ -34,7 +35,8 @@ spirograph <- function(
   origin_y = 0,
   rotation = 0,
   start_angle = 0,
-  points_per_polygon = round(abs(1000 * windings),0),  rule = "evenodd",
+  points_per_polygon = round(abs(1000 * windings),0),
+  rule = "evenodd",
   openfile = TRUE,
   aspect_ratio = 1,
   end_at_beginning = F,
@@ -98,15 +100,15 @@ spirograph <- function(
                                              default = 1)))
       d_transitions <- dplyr::mutate(
         d_transitions,
-        color_id = if_else(transition,
-                           lag(color_id, default = 1),
+        color_id = dplyr::if_else(transition,
+                                  dplyr::lag(color_id, default = 1),
                            color_id),
-        color_cycle_id = if_else(transition,
-                                 lag(color_cycle_id,
+        color_cycle_id = dplyr::if_else(transition,
+                                        dplyr::lag(color_cycle_id,
                                      default = 1),
                                  color_cycle_id),
-        id = if_else(transition,
-                     lag(id, default = 1),
+        id = dplyr::if_else(transition,
+                            dplyr::lag(id, default = 1),
                      id))
       d_transitions <- dplyr::filter(d_transitions,
                                      transition)
@@ -135,12 +137,12 @@ if (end_at_beginning) {
 
           }
 
-    nd <- nest(group_by(d, color_cycle_id, color_id))
-    nd <- mutate(nd, col = scales::alpha(colors[color_id],
+    nd <- tidyr::nest(dplyr::group_by(d, color_cycle_id, color_id))
+    nd <- dplyr::mutate(nd, col = scales::alpha(colors[color_id],
                                          alpha = transparency))
-    nd <- rename(nd, x = data)
+    nd <- dplyr::rename(nd, x = data)
 
-    purrr::pwalk(select(nd, x, col), polypath,
+    purrr::pwalk(dplyr::select(nd, x, col), graphics::polypath,
                  border = NA,
                  rule = rule)
 
